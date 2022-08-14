a3c5d49dbf1e56bff5135b9c8b9cdef1f0da97bb
Sadhna kumari <sadhhnakhairia1998@gmail.com>
2022-08-14 17:16:34 +0530

    change

--- main.js
+++ main.js
@@ -1,29 +1,38 @@
-let taskBtn = document.querySelector(".task");
+const btnAll = document.querySelector(".all");
+const btnRed = document.querySelector(".red");
+const btnYellow = document.querySelector(".yellow");
+const btnBlue = document.querySelector(".blue");
+const btnBlack = document.querySelector(".black");
+const btnTick = document.querySelector(".tick");
 
-let modalContainer = document.querySelector(".modal_container");
+const lastContainer = document.querySelector(".end_tasks");
+const lastRed = document.querySelector(".red_task");
+const lastYellow = document.querySelector(".yellow_task");
+const lastBlue = document.querySelector(".blueTask");
+const lastBlack = document.querySelector(".black_task");
+const lastComp = document.querySelector(".complete_final");
 
-const colorBtn = document.querySelectorAll(".clrbtn");
 
-const doneBtn = document.querySelector(".done_btn");
+const modalContainer = document.querySelector(".modal_container");
 
+const todo = document.querySelectorAll("#todo");
+const cancelBtn = document.querySelector(".cancel_btn")
 const textArea = document.querySelector(".text_area");
 
-const contentArea = document.querySelector(".content");
+const insideRed = document.querySelector(".red_btn");
+const insideYellow = document.querySelector(".yellow_btn");
+const insideBlue = document.querySelector(".blue_btn");
+const insideBlack = document.querySelector(".black_btn");
 
+const createTask = document.querySelector(".task");  //btnform
+const overlap = document.querySelector(".content");  //overlay
 
+createTask.addEventListener("click", () => {
+  modalContainer.classList.add("active");
+});
+const doneBtn = document.querySelector(".done_btn");
 
+doneBtn.addEventListener("click", function (e) {
+  modalContainer.classList.remove("active");
+});
 
-let cancelBtn = document.querySelector(".cancel_btn")
-
-taskBtn.addEventListener("click", () => {
-    modal.classList.add("active");
-  });
-
-  cancelBtn.addEventListener("click", () => {
-    modal.classList.remove("active");
-  });
-
-  // =====Selectors====== 
-const mainCont = document.querySelector(".task-cont");
-const priorityContainer = document.querySelector(".priority-container");
-const taskItem = document.querySelectorAll(".task-item");
